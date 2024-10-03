6ae3ee9e1db169ab41caccd920ff77087277ba79
Alishh <19amuhammad@keshacademy.com>
2024-10-03 10:04:45 +0100

    Create coin_task.py

---
+++ coin_task.py
@@ -0,0 +1,19 @@
+one = 0.01
+two = 0.02
+five = 0.05
+ten = 0.1
+twenty = 0.2
+fifty = 0.5
+one_pound = 1
+two_pound = 2
+
+onep = int(input("How many 1p coins do you have? ")
+twop = int(input("How many 2p coins do you have? ")
+fivep = int(input("how many 5p coins do you have? ")
+tenp = int(input("how many 10p coins do you have? ")
+twentyp = int(input("how many 20p coins do you have? ")
+fiftyp = int(input("how many 50p coins do you have? ")
+onepound = int(input("how many 1 pound coins do you have? ")
+twopound = int(input("how many 2 pound coins do you have? ")
+
+print ("you have")
